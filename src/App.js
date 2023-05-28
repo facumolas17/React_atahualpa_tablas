0d01118b86132543ac7908cb7500dd3d813a2cc6
Facu Molas <facundomolasymolas@gmail.com>
2023-05-28 20:00:48 +0200

    cree rutas

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import './App.css';
 import NavBar from './components/NavBar';
 import NavEncabezado from './components/NavEncabezado';
 import ItemListContainer from './components/ItemListContainer';
-
+import ItemDetailContainer from './components/ItemDetailContainer';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
       <NavEncabezado/>
       <NavBar/>
       <Portada/>
-      <ItemListContainer/>
-      
-    </div>
+      <Routes>
+        <Route path='/' element={<ItemListContainer/>} />
+        <Route path='/producto' element={<ItemDetailContainer/>} />  
+      </Routes>
+    </BrowserRouter>
   );
 }
 
